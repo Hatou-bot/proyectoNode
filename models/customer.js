713bece30f53647bcc778413658a425617c9dfe9
pgarcia92 <palomaglorenzo@gmail.com>
2020-06-09 17:57:44 +0200

    Did a bunch of bassic stuff. I'm bad at comments ok

---
+++ models/customer.js
@@ -0,0 +1,14 @@
+const queries = require('../queries')
+
+const getAll = () => {
+    return new promise((resolve, reject) => {
+        db.query(queries.selectAll, (err, row) => {
+            if(err) reject (err)
+            resolve(rows)
+        })
+    })
+}
+
+module.exports = {
+    getAll
+}
