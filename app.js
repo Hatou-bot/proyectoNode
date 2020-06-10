fb0646f8aaebc8af276e32cd414ee72f86499fb5
Ana Junquera Vara <anajunqueravs@gmail.com>
2020-06-10 17:20:55 +0200

    no message
    Signed-off-by: Ana Junquera Vara <anajunqueravs@gmail.com>

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const dotevn = require ('dotenv');
 const cors = require('cors')
 
 const indexRouter = require('./routes/index');
